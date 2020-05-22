69771ec371f53ff08ca71f3ce0d2ded45c0b1c9a
santhosh0209 <bluyblack0902@gmail.com>
2020-05-22 20:32:48 +0530

    initial commit

--- event_management/settings.py
+++ event_management/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'Whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'event_management.urls'
